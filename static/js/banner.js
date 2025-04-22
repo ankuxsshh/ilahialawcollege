e84b10327c48ae439574e3673c07fee19ac83ab0
ankuxsshh <ankuxshh72@gmail.com>
2025-04-22 15:04:42 +0530

    lastupdates

---
+++ static/js/banner.js
@@ -0,0 +1,18 @@
+
+const images = [
+    "static/images/banner1.jpg",
+    "static/images/banner2.jpg",
+    "static/images/banner3.jpg"
+];
+
+let index = 0;
+const slideshow = document.getElementById("slideshow-bg");
+
+setInterval(() => {
+    index = (index + 1) % images.length;
+    slideshow.style.opacity = 0;
+    setTimeout(() => {
+        slideshow.src = images[index];
+        slideshow.style.opacity = 1;
+    }, 1000); // Duration matches the transition
+}, 5000); // Change image every 5 seconds
